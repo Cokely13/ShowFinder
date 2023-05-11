5dc0b4c3b5e1f4adb430b775395b5d4b6f86efd3
Ryan Cokely <ryan.cokely@gmail.com>
2023-05-11 14:46:47 -0400

    test

--- script/seed.js
+++ script/seed.js
@@ -140,7 +140,7 @@
 // module.exports = seed
 
 const { db, models: { User, Show } } = require('../server/db');
-const Axios = require('Axios');
+const axios = require('axios').default;
 
 const baseURL = 'http://api.tvmaze.com';
 const API_KEY = '9BNGjUxBbrFaisuV-xKIATvtUuKaZYG9';
@@ -161,7 +161,7 @@ async function seed() {
 
   // Creating TV Shows
   for (let page = 0; page < TOTAL_SHOWS / PAGE_SIZE; page++) {
-    const response = await Axios.get(`${baseURL}/shows`, {
+    const response = await axios.get(`${baseURL}/shows`, {
       params: {
         page,
         limit: PAGE_SIZE,
